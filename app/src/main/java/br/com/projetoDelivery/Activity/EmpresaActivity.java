d521046b93f5f63eb85460e2db001c8b5d689583
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-08 23:48:57 -0300

    Criação de Toolbar

--- app/src/main/java/br/com/projetoDelivery/Activity/EmpresaActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/EmpresaActivity.java
@@ -3,14 +3,27 @@ package br.com.projetoDelivery.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.projetoDelivery.Helper.ConfigFireBase;
 import br.com.projetoDelivery.R;
 
 public class EmpresaActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        autenticacao = ConfigFireBase.getFirebaseAutenticacao();
+
+        //Configurações Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPadrao);
+        toolbar.setTitle("Ifood - empresa");
+        //setSupportActionBar(toolbar);
     }
 }
