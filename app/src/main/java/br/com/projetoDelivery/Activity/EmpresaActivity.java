aa38bc92184450060233f7f225ccb1938294e55d
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-09 21:15:02 -0300

    Correção
    Função DeslogarUsuario

--- app/src/main/java/br/com/projetoDelivery/Activity/EmpresaActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/EmpresaActivity.java
@@ -57,7 +57,7 @@ public class EmpresaActivity extends AppCompatActivity {
     private void deslogarUsuario(){
         try{
             autenticacao.signOut();
-
+            finish();
         }catch (Exception e){
             e.printStackTrace();
         }
