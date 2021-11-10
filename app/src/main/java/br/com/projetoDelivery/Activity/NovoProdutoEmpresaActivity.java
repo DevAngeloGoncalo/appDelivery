36cd2975d1ed318704bfc783c6eeb47f7d714c78
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-09 21:04:46 -0300

    Toobar Adcionado Activity NovoProduto

--- app/src/main/java/br/com/projetoDelivery/Activity/NovoProdutoEmpresaActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/NovoProdutoEmpresaActivity.java
@@ -1,6 +1,7 @@
 package br.com.projetoDelivery.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,11 @@ public class NovoProdutoEmpresaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_produto_empresa);
+
+        //Configurações Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPadrao);
+        toolbar.setTitle("Novo Produto");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
