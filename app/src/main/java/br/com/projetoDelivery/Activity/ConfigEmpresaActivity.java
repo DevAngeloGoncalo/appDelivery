9af59cecb80ea55dcf93fd37e169cb419ea22372
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-12 02:00:42 -0300

    Materialiazar funcionando com foto do perfil

--- app/src/main/java/br/com/projetoDelivery/Activity/ConfigEmpresaActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/ConfigEmpresaActivity.java
@@ -16,12 +16,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
 
@@ -32,11 +39,12 @@ import br.com.projetoDelivery.Helper.UsuarioFireBase;
 import br.com.projetoDelivery.R;
 
 public class ConfigEmpresaActivity extends AppCompatActivity {
-    //pensar na possibilidade de tirar uma foto para usuario
+
     private static final int SELECIONAR_GALERIA = 200;
 
     //Firebase
     private StorageReference storageReference;
+    private DatabaseReference firebaseRef;
 
     private String idUsuarioLogado;
     private String urlImagemEscolhida = "";
@@ -49,8 +57,6 @@ public class ConfigEmpresaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_config_empresa);
 
         inicializarComponentes();
-        storageReference = ConfigFireBase.getFirebaseStorage();
-        idUsuarioLogado = UsuarioFireBase.getIdUsuario();
 
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPadrao);
@@ -69,6 +75,8 @@ public class ConfigEmpresaActivity extends AppCompatActivity {
                 }
             }
         });
+
+        materializarDadosEmpresa();
     }
 
     @Override
@@ -96,26 +104,49 @@ public class ConfigEmpresaActivity extends AppCompatActivity {
 
                     //Upload
                     UploadTask uploadTask= imagemRef.putBytes(dadosImg);
-                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                         @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(ConfigEmpresaActivity.this, "ERRO AO FAZER UPLOAD DA IMAGEM",Toast.LENGTH_SHORT).show();
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()){
+                                throw task.getException();
+                            }
+                            return imagemRef.getDownloadUrl();
                         }
-                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                         @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            urlImagemEscolhida  = taskSnapshot.getStorage().getDownloadUrl().toString();
-
-                            imagemRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Uri> task) {
-                                    Uri url = task.getResult();
-                                }
-                            });
-
-                            Toast.makeText(ConfigEmpresaActivity.this, "SUCESSO AO FAZER UPLOAD DA IMAGEM",Toast.LENGTH_SHORT).show();
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Uri downloadUrl = task.getResult();
+                                urlImagemEscolhida = downloadUrl.toString();
+                                Toast.makeText(ConfigEmpresaActivity.this, "Sucesso ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(ConfigEmpresaActivity.this, "Erro ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     });
+
+//                    uploadTask.addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(ConfigEmpresaActivity.this, "ERRO AO FAZER UPLOAD DA IMAGEM",Toast.LENGTH_SHORT).show();
+//                        }
+//                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            //urlImagemEscolhida  = taskSnapshot.getStorage().getDownloadUrl().toString();
+//
+//
+//                            imagemRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Uri> task) {
+//                                    Uri url = task.getResult();
+//
+//                                }
+//                            });
+//
+//                            Toast.makeText(ConfigEmpresaActivity.this, "SUCESSO AO FAZER UPLOAD DA IMAGEM",Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
 
                 }
 
@@ -132,6 +163,12 @@ public class ConfigEmpresaActivity extends AppCompatActivity {
         editEmpresaNome = findViewById(R.id.editEmpresaNome);
         editEmpresaCategoria = findViewById(R.id.editEmpresaCategoria);
         imagePerfilEmpresa = findViewById(R.id.imagePerfilEmpresa);
+
+        storageReference = ConfigFireBase.getFirebaseStorage();
+        firebaseRef = ConfigFireBase.getFirebase();
+        idUsuarioLogado = UsuarioFireBase.getIdUsuario();
+
+        Picasso.get().setLoggingEnabled(true);
     }
 
     private void exibirMensagem(String texto){
@@ -174,4 +211,37 @@ public class ConfigEmpresaActivity extends AppCompatActivity {
         }
 
     }
+
+    private void materializarDadosEmpresa(){
+
+        DatabaseReference empresaRef = firebaseRef.child("empresas").child(idUsuarioLogado);
+        empresaRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.getValue() != null){
+                    Empresa empresa = snapshot.getValue(Empresa.class);
+                    editEmpresaCategoria.setText(empresa.getCategoria());
+                    editEmpresaEntregaTaxa.setText(empresa.getPrecoEntrega().toString());
+                    editEmpresaEntregaTempo.setText(empresa.getTempo());
+                    editEmpresaNome.setText(empresa.getNome());
+
+                    urlImagemEscolhida = empresa.getUrlImagem();
+
+                    if (urlImagemEscolhida != ""){
+                        //Glide.with(getApplication()).load(urlImagemEscolhida).into(imagePerfilEmpresa);
+                        Picasso.get().load(urlImagemEscolhida).into(imagePerfilEmpresa);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
