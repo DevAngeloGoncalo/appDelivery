ee499796ac7b6c144d92597f912966c68d15b3aa
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-12 02:16:59 -0300

    Criando seção de produtos

--- app/src/main/java/br/com/projetoDelivery/Activity/NovoProdutoEmpresaActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/NovoProdutoEmpresaActivity.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import br.com.projetoDelivery.Helper.ConfigFireBase;
+import br.com.projetoDelivery.Helper.UsuarioFireBase;
+import br.com.projetoDelivery.Model.Empresa;
 import br.com.projetoDelivery.R;
 
 public class NovoProdutoEmpresaActivity extends AppCompatActivity {
+
+    private EditText editProdutoNome, editProdutoDescricao, editProdutoPreco;
+    private ImageView imageProduto;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +33,42 @@ public class NovoProdutoEmpresaActivity extends AppCompatActivity {
         toolbar.setTitle("Novo Produto");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //public para ser acessado do botao
+    public void validarDadosProduto(View view) {
+        String nome = editProdutoNome.getText().toString();
+        String descricao = editProdutoDescricao.getText().toString();
+        String preco = editProdutoPreco.getText().toString();
+
+        if (!nome.isEmpty()) {
+            if (!descricao.isEmpty()) {
+                if (!preco.isEmpty()) {
+
+                }else{
+                    exibirMensagem("Digite o preço do produto");
+                }
+            }else{
+                exibirMensagem("Digite uma descrição");
+            }
+
+        }else{
+            exibirMensagem("Digite um nome do produto");
+        }
+
+    }
+
+    private void exibirMensagem(String texto){
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void inicializarComponentes(){
+        editProdutoDescricao = findViewById(R.id.editProdutoDescricao);
+        editProdutoNome = findViewById(R.id.editProdutoNome);
+        editProdutoPreco = findViewById(R.id.editProdutoPreco);
+
+        imageProduto = findViewById(R.id.imageProduto);
+
     }
 }
---
+++ app/src/main/java/br/com/projetoDelivery/Model/Produto.java
@@ -0,0 +1,4 @@
+package br.com.projetoDelivery.Model;
+
+public class Produto {
+}
