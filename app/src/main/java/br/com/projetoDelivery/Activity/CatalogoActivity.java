4d985288f7ce93c46e62b11a9014fae26a905686
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-15 21:29:17 -0300

    Mostrar imagem e foto da empresa ao clicar

--- app/src/main/java/br/com/projetoDelivery/Activity/CatalogoActivity.java
+++ app/src/main/java/br/com/projetoDelivery/Activity/CatalogoActivity.java
@@ -2,12 +2,23 @@ package br.com.projetoDelivery.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import br.com.projetoDelivery.Model.Empresa;
 import br.com.projetoDelivery.R;
 
 public class CatalogoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerProdutosCatalogo;
+    private ImageView imageEmpresaCatalogo;
+    private TextView textNomeEmpresaCatalogo;
+    private Empresa empresaEscolhida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +26,28 @@ public class CatalogoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_catalogo);
 
         inicializarComponentes();
+
+        //identificar empresa escolhida
+        identificarEmpresaEscolhida();
+    }
+
+    private void identificarEmpresaEscolhida(){
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            empresaEscolhida = (Empresa) bundle.getSerializable("empresa");
+
+            textNomeEmpresaCatalogo.setText(empresaEscolhida.getNome());
+
+            String url = empresaEscolhida.getUrlImagem();
+            Picasso.get().load(url).into(imageEmpresaCatalogo);
+        }
     }
 
     private void inicializarComponentes(){
+        recyclerProdutosCatalogo = findViewById(R.id.recyclerProdutosCatalogo);
+        textNomeEmpresaCatalogo = findViewById(R.id.textNomeEmpresaCatalogo);
+        imageEmpresaCatalogo = findViewById(R.id.imageEmpresaCatalogo);
+
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Produtos Disponíveis");
