5bf95331d75cc02a0927fccfb2bd7a71a926e162
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-11-17 22:41:39 -0300

    Configurando Pedidos

---
+++ app/src/main/java/br/com/projetoDelivery/Activity/PedidosActivity.java
@@ -0,0 +1,54 @@
+package br.com.projetoDelivery.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+
+import br.com.projetoDelivery.Helper.ConfigFireBase;
+import br.com.projetoDelivery.Helper.UsuarioFireBase;
+import br.com.projetoDelivery.R;
+
+public class PedidosActivity extends AppCompatActivity {
+    private RecyclerView recyclerPedidos;
+    private DatabaseReference firebaseRef;
+    private String idEmpresa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos);
+
+        inicializarComponentes();
+
+        materializarPedidos();
+    }
+
+    private void materializarPedidos(){
+
+    }
+
+    private void inicializarComponentes(){
+        //Configurações Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Pedidos");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerPedidos = findViewById(R.id.recyclerPedidos);
+        firebaseRef = ConfigFireBase.getFirebase();
+        idEmpresa = UsuarioFireBase.getIdUsuario();
+
+        //Configurar RecyclerView
+        recyclerPedidos.setLayoutManager(new LinearLayoutManager(this));
+        recyclerPedidos.setHasFixedSize(true);
+//        adapterPedido = new AdapterPedido(pedidos);
+//        recyclerPedidos.setAdapter(adapterPedido);
+
+
+    }
+}
